d4c8efd067c687c30b7dfcc1e986cabb0de5b804
Perez55 <93294139+Perez55@users.noreply.github.com>
2023-01-12 17:54:58 -0500

    Update

--- Lab1.js
+++ Lab1.js
@@ -1,4 +1,3 @@
-console.log("Hilbert Perez"); //Example of console message working
-console.log("Hello my name is Hilbert Perez");
-
 /** The purpose of this Lab is to practice and understand Linking javascript files from a HTML file */
+//console.log("Hilbert Perez"); 
+
